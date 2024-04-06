4ac1f081a3ae157283dcdf2c6426031312e300e1
Laszlo Pinter <lpinter99@gmail.com>
2024-04-05 20:53:19 -0700

    Refactored Settings to make it more readable

--- NoSnow/Setting.cs
+++ NoSnow/Setting.cs
@@ -11,27 +11,35 @@ namespace NoSnow
     [FileLocation(nameof(NoSnow))]
 
     // Not referenced groups do not have to be commented out, but better to make the code cleaner
-    [SettingsUIGroupOrder(kToggleGroup, kSliderGroup)]
+    [SettingsUIGroupOrder(kToggleGroupPrecipitation, kSliderGroupTemperature)]
     // [SettingsUIGroupOrder(kButtonGroup, kToggleGroup, kSliderGroup, kDropdownGroup)]
 
     // Not referenced groups do not have to be commented out, but better to make the code cleaner
-    [SettingsUIShowGroupName(kToggleGroup, kSliderGroup)]
+    [SettingsUIShowGroupName(kToggleGroupPrecipitation, kSliderGroupTemperature)]
     // [SettingsUIShowGroupName(kButtonGroup, kToggleGroup, kSliderGroup, kDropdownGroup)]
     public class Setting : ModSetting
     {
         public const string kSection = "Main";
 
-        // Not referenced groups do not have to be commented out, but better to make the code cleaner
-        // public const string kButtonGroup = "Button";
-        public const string kToggleGroup = "Toggle";
-        public const string kSliderGroup = "Slider";
-        // Not referenced groups do not have to be commented out, but better to make the code cleaner
-        // public const string kDropdownGroup = "Dropdown";
-
         public Setting(IMod mod) : base(mod)
         {
 
         }
+
+        // -----------------------------------------------------------------
+        // Group definitions
+
+        public const string kToggleGroupPrecipitation = "Toggle";
+        public const string kSliderGroupTemperature = "Slider";
+
+        // Not referenced groups do not have to be commented out, but better to make the code cleaner
+        /*
+        public const string kButtonGroup = "Button";
+        public const string kDropdownGroup = "Dropdown";
+        */
+
+        // -----------------------------------------------------------------
+        // Button control definition
 
         // Commented out here to disable the display of the control
         // Also, commented out the references in ReadEntries below
@@ -39,6 +47,9 @@ namespace NoSnow
         [SettingsUISection(kSection, kButtonGroup)]
         public bool Button { set { Mod.log.Info("Button clicked"); } }
         */
+
+        // -----------------------------------------------------------------
+        // Buton with confirmation control definition
 
         // Commented out here to disable the display of the control
         // Also, commented out the references in ReadEntries below
@@ -49,12 +60,21 @@ namespace NoSnow
         public bool ButtonWithConfirmation { set { Mod.log.Info("ButtonWithConfirmation clicked"); } }
         */
 
-        [SettingsUISection(kSection, kToggleGroup)]
-        public bool Toggle { get; set; }
+        // -----------------------------------------------------------------
+        // Disable snow toggle control definition
+
+        [SettingsUISection(kSection, kToggleGroupPrecipitation)]
+        public bool ToggleDisableSnow { get; set; }
+
+        // -----------------------------------------------------------------
+        // Set minimum temparature integer slider control
 
         [SettingsUISlider(min = -50, max = 50, step = 1, scalarMultiplier = 1, unit = Unit.kTemperature)]
-        [SettingsUISection(kSection, kSliderGroup)]
-        public int IntSlider { get; set; }
+        [SettingsUISection(kSection, kSliderGroupTemperature)]
+        public int IntSliderMinimumTemperature { get; set; }
+
+        // -----------------------------------------------------------------
+        // Integer dropdown control definition
 
         // Commented out here to disable the display of the control
         // Also, commented out the references in ReadEntries below
@@ -62,17 +82,8 @@ namespace NoSnow
         [SettingsUIDropdown(typeof(Setting), nameof(GetIntDropdownItems))]
         [SettingsUISection(kSection, kDropdownGroup)]
         public int IntDropdown { get; set; }
-        */
 
-        // Commented out here to disable the display of the control
-        // Also, commented out the references in ReadEntries below
-        /*
-        [SettingsUISection(kSection, kDropdownGroup)]
-        public SomeEnum EnumDropdown { get; set; } = SomeEnum.Value1;
-        */
-
-        // Not used
-        /*
+        // Load the integer values into the dropdown
         public DropdownItem<int>[] GetIntDropdownItems()
         {
             var items = new List<DropdownItem<int>>();
@@ -90,13 +101,16 @@ namespace NoSnow
         }
         */
 
-        public override void SetDefaults()
-        {
-            throw new System.NotImplementedException();
-        }
+        // -----------------------------------------------------------------
+        // Enum dropdown control definition
 
-        // Not used
+        // Commented out here to disable the display of the control
+        // Also, commented out the references in ReadEntries below
         /*
+        [SettingsUISection(kSection, kDropdownGroup)]
+        public SomeEnum EnumDropdown { get; set; } = SomeEnum.Value1;
+
+        // Load the enum values into the dropdown
         public enum SomeEnum
         {
             Value1,
@@ -104,6 +118,14 @@ namespace NoSnow
             Value3,
         }
         */
+
+        // -----------------------------------------------------------------
+
+        public override void SetDefaults()
+        {
+            throw new System.NotImplementedException();
+        }
+
     }
 
     public class LocaleEN : IDictionarySource
@@ -117,27 +139,38 @@ namespace NoSnow
         {
             return new Dictionary<string, string>
             {
+                // -----------------------------------------------------------------
+                // Tab title localization
+
                 // This is the name of the tab in the Options menu. Make it the same as the name of the mod.
                 { m_Setting.GetSettingsLocaleID(), "No Snow" },
 
                 // I am not sure what the effect of this setting
                 { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main" },
 
+                // -----------------------------------------------------------------
+                // Group localization
+
+                { m_Setting.GetOptionGroupLocaleID(Setting.kToggleGroupPrecipitation), "Precipitation" },
+                { m_Setting.GetOptionGroupLocaleID(Setting.kSliderGroupTemperature), "Temperature" },
+
                 // Not referenced groups do not have to be commented out, but better to make the code cleaner
                 /*
                 { m_Setting.GetOptionGroupLocaleID(Setting.kButtonGroup), "Buttons" },
-                */
-                { m_Setting.GetOptionGroupLocaleID(Setting.kToggleGroup), "Snow" },
-                { m_Setting.GetOptionGroupLocaleID(Setting.kSliderGroup), "Temperature" },
-                /*
                 { m_Setting.GetOptionGroupLocaleID(Setting.kDropdownGroup), "Dropdowns" },
                 */
+
+                // -----------------------------------------------------------------
+                // Button control localization
 
                 // Commented out as the control in Setting above is not displayed
                 /*
                 { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Button)), "Button" },
                 { m_Setting.GetOptionDescLocaleID(nameof(Setting.Button)), $"Simple single button. It should be bool property with only setter or use [{nameof(SettingsUIButtonAttribute)}] to make button from bool property with setter and getter" },
                 */
+
+                // -----------------------------------------------------------------
+                // Buton with confirmation control localization
 
                 // Commented out as the control in Setting above is not displayed
                 /*
@@ -146,17 +179,29 @@ namespace NoSnow
                 { m_Setting.GetOptionWarningLocaleID(nameof(Setting.ButtonWithConfirmation)), "is it confirmation text which you want to show here?" },
                 */
 
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Toggle)), "Disable snow" },
-                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Toggle)), $"Check to disable snow fall" },
+                // -----------------------------------------------------------------
+                // Disable snow toggle control localization
 
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.IntSlider)), "The minimum temperature" },
-                { m_Setting.GetOptionDescLocaleID(nameof(Setting.IntSlider)), $"Set the minimum temperature value" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.ToggleDisableSnow)), "Disable snow" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.ToggleDisableSnow)), $"Check to disable snow fall" },
+
+                // -----------------------------------------------------------------
+                // Set minimum temparature integer slider control localization
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.IntSliderMinimumTemperature)), "The minimum temperature" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.IntSliderMinimumTemperature)), $"Set the minimum temperature value" },
+
+                // -----------------------------------------------------------------
+                // Integer dropdown control localization
 
                 // Commented out as the control in Setting above is not displayed
                 /*
                 { m_Setting.GetOptionLabelLocaleID(nameof(Setting.IntDropdown)), "Int dropdown" },
                 { m_Setting.GetOptionDescLocaleID(nameof(Setting.IntDropdown)), $"Use int property with getter and setter and [{nameof(SettingsUIDropdownAttribute)}(typeof(SomeType), nameof(SomeMethod))] to get int dropdown: Method must be static or instance of your setting class with 0 parameters and returns {typeof(DropdownItem<int>).Name}" },
                 */
+
+                // -----------------------------------------------------------------
+                // Enum dropdown control localization
 
                 // Commented out as the control in Setting above is not displayed
                 /*
@@ -169,7 +214,7 @@ namespace NoSnow
                 */
             };
 
-            // Original dictionary
+            // Original localization dictionary
             /*
             return new Dictionary<string, string>
             {
